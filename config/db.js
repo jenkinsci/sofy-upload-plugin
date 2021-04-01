3eae829a394622a11694ffea412f9e7d60a19547
Saad Ahmed <saad@sofy.ai>
2021-04-01 19:58:10 +0500

    token fix

--- config/db.js
+++ config/db.js
@@ -5,7 +5,11 @@ const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   server: process.env.DB_HOST,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  options: {
+    "encrypt": true,
+    "enableArithAbort": true
+  }
 };
 
 const poolPromise = new sql.ConnectionPool(config);
