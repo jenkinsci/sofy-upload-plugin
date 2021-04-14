724702d398de02e6de4e29ff436b8413fc22881a
Taha Muhammad <taha@sofy.ai>
2021-04-14 13:42:23 +0500

    eslint fixes

--- routes/subscription.route.js
+++ routes/subscription.route.js
@@ -1,15 +1,14 @@
 const Joi = require('joi');
-const router = require("express").Router();
-const { subscription: { btTokenGeneration }, } = require("../controller");
-const { subscription: { createSubscription }, } = require("../controller");
-const { subscription: { cancelSubscription }, } = require("../controller");
-const { subscription: { brainTreeWebHookHandler }, } = require("../controller");
+const router = require('express').Router();
+const { subscription: { btTokenGeneration } } = require('../controller');
+const { subscription: { createSubscription } } = require('../controller');
+const { subscription: { cancelSubscription } } = require('../controller');
+const { subscription: { brainTreeWebHookHandler } } = require('../controller');
 const { validateSchema } = require('../middlewares');
 
-
-//Step 1 (Payment initiation): Sofy client (web) requests user specific token from Braintree server via sofy server by sending user's guid.
-router.get("/getClientToken/:userGuid", async (req, res, next) => {
-  const userGuid = req.params.userGuid;
+// Step 1 (Payment initiation): Sofy client (web) requests user specific token from Braintree server via sofy server by sending user's guid.
+router.get('/getClientToken/:userGuid', async (req, res, next) => {
+  const { userGuid } = req.params;
   try {
     const token = await btTokenGeneration(userGuid);
     res.json(token);
@@ -23,60 +22,59 @@ router.get("/getClientToken/:userGuid", async (req, res, next) => {
 2)  planId: Plan BTreeCode code on which user wants to subscribe.
 3)  user_guid: Unique identifier of a user.
 */
-router.post("/createSubscription",
+router.post('/createSubscription',
   validateSchema({
     body: Joi.object({
       paymentMethodNonce: Joi.string().required(),
       planId: Joi.string().required(),
-      user_guid: Joi.string().required()
+      user_guid: Joi.string().required(),
     }),
   }), async (req, res, next) => {
-    const{
-      paymentMethodNonce, planId, user_guid
+    const {
+      paymentMethodNonce, planId, user_guid,
     } = req.body;
     try {
-      var status = await createSubscription(paymentMethodNonce, planId, user_guid);
+      const status = await createSubscription(paymentMethodNonce, planId, user_guid);
       res.json({
         message: 'Subscription Successfull',
         status,
       });
     } catch (error) {
-        next(error);
+      next(error);
     }
   });
 
-//api to cancell the currently active subscription on Braintree
-router.post("/cancel/:userGuid", async (req, res, next) => {
-    var userGuid = req.params.userGuid;
+// api to cancell the currently active subscription on Braintree
+router.post('/cancel/:userGuid', async (req, res, next) => {
+  const { userGuid } = req.params;
+  try {
+    const status = await cancelSubscription(userGuid);
+    res.json({
+      message: 'Subscription Cancelled',
+      status,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// api to listen to Braintree webhooks
+router.post('/handleBraintreeWebhook',
+  validateSchema({
+    body: Joi.object({
+      bt_signature: Joi.string().required(),
+      bt_payload: Joi.string().required(),
+    }),
+  }), async (req, res, next) => {
+    const { bt_signature, bt_payload } = req.body;
     try {
-      var status = await cancelSubscription(userGuid);
+      const status = await brainTreeWebHookHandler(bt_signature, bt_payload);
       res.json({
-        message: 'Subscription Cancelled',
         status,
       });
     } catch (error) {
       next(error);
     }
   });
-
-  //api to listen to Braintree webhooks
-  router.post("/handleBraintreeWebhook",
-  validateSchema({
-    body: Joi.object({
-      bt_signature: Joi.string().required(),
-      bt_payload: Joi.string().required()
-    }),
-  }), async (req, res, next) => {
-    const { bt_signature, bt_payload } = req.body;
-    try {
-      var status = await brainTreeWebHookHandler(bt_signature, bt_payload);
-      res.json({
-        status
-      });
-    } catch (error) {
-      next(error);
-    }
-  });
-  
 
 module.exports = router;
