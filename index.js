653b32fef95c026b2355a33f8faa251ebd613319
Taha Muhammad <taha.muhammad.official@gmail.com>
2021-08-12 17:22:01 +0500

    fixes in basic structure

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
 require('module-alias/register');
+require('dotenv').config();
 
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const db = require('./config/db');
-require('./config/sequelize');
+const ExpressServer = require('./lib/app');
+const config = require('./lib/config');
 
-const passport = require('./config/passport');
-const routes = require('./lib/routes');
-
-(async () => {
-    dotenv.config();
-    await db.connect();
-
-    const app = express();
-    passport(app);
-
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(cors());
-    app.use(routes.AuthRouter);
-
-    app.use((err, req, res, next) => {
-        console.error(err);
-        res.status(err.statusCode || 500).json({
-            message: err.message || 'Something went wrong!',
-        });
-    });
-
-    app.listen(8000, () => console.log('Sofy server is up and running on port 8000'));
-})();
+new ExpressServer(config).setup();
